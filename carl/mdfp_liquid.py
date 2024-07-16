63789d4ad1e46412ab46afb0532da1327a10c474
cschiebroek <carl.schiebroek@phys.chem.ethz.ch>
2024-07-16 14:49:30 +0200

    set-up db

--- carl/mdfp_liquid.py
+++ carl/mdfp_liquid.py
@@ -1,5 +1,5 @@
 #mdfptools
-from carl.liquid_phase_experiments.Parameteriser import LiquidParameteriser
+from Parameteriser import LiquidParameteriser
 from Simulator import LiquidSimulator
 from Composer import LiquidComposer
 
@@ -45,20 +45,20 @@ mdfp = LiquidComposer.run(traj, rdk_pmd,smiles=smiles)
 mdfp = mdfp.get_mdfp()
 mdfp_dict = {'mdfp':str(mdfp)}
 print(mdfp)
-# print('Mdfp composed, saving to database...')
-# Mdfp_Experiment_uuid = Md_Experiment_uuid
-# mdfp_conf_uuid = uuid.uuid4()
+print('Mdfp composed, saving to database...')
+Mdfp_Experiment_uuid = Md_Experiment_uuid
+mdfp_conf_uuid = uuid.uuid4()
 
-# print('Connecting to database...')
-# hostname = 'scotland'
-# dbname = 'cs_mdfps'
-# username = 'cschiebroek'
-# cn = psycopg2.connect(host=hostname, dbname=dbname, user=username)
-# cur = cn.cursor()
-# #retrieve conformer from database by confid
-# cur.execute("insert into cs_mdfps_schema.mdfp_experiment_data values (%s, %s, %s,%s,%s)",(str(confid), str(Mdfp_Experiment_uuid), json.dumps(mdfp_dict),str(mdfp_conf_uuid),Md_Experiment_uuid))
-# print(mdfp)
-# cn.commit()
-# print('Data saved, closing connection...')
-# cn.close()
-# print('Connection closed, exiting...')
+print('Connecting to database...')
+hostname = 'scotland'
+dbname = 'cs_mdfps'
+username = 'cschiebroek'
+cn = psycopg2.connect(host=hostname, dbname=dbname, user=username)
+cur = cn.cursor()
+#retrieve conformer from database by confid
+cur.execute("insert into cs_mdfps_schema.mdfp_experiment_data values (%s, %s, %s,%s,%s)",(str(confid), str(Mdfp_Experiment_uuid), json.dumps(mdfp_dict),str(mdfp_conf_uuid),Md_Experiment_uuid))
+print(mdfp)
+cn.commit()
+print('Data saved, closing connection...')
+cn.close()
+print('Connection closed, exiting...')
